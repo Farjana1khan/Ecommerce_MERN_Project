85657cb0a61ee9b2f6965631bf3e6e6af488fea5
Farjana Khan <farjana09khan@gmail.com>
2022-11-22 17:09:45 +0530

    update server

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,7 @@ cloudinary.config(
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
-   }
-
-
-)
+   })
 
 
 //Connect Database
