076c16d5bc7343ceb3ba7bf3bc2447d0276a4c2f
Farjana Khan <farjana09khan@gmail.com>
2022-11-22 18:59:58 +0530

    changes in app.js file

--- backend/app.js
+++ backend/app.js
@@ -60,7 +60,7 @@ app.use("/api/v1", payment)
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
 });
 
 
