2393f607b1484a01326b66cdca5ed22a2fbe2c35
Farjana Khan <farjana09khan@gmail.com>
2022-11-23 10:20:00 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -64,7 +64,7 @@ app.get((req,res)=>{
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 
 
